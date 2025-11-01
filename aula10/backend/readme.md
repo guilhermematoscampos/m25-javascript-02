4c8d73fafff990640d996f4b587fcc03e22eb7a8
guilherme  matos campos <guilhermematoscampos@gmail.com>
2025-11-01 10:44:24 -0300

    aula 10

---
+++ aula10/backend/readme.md
@@ -0,0 +1,11 @@
+
+//node package manager
+npm init -y
+npx gitignore node (cria gitignore)
+npm install json-server
+
+no package.json {
+    "start":"npx json-server --watch db.json --port 3000"
+}
+
+npm start
---
+++ aula10/frontend/js/cadastro.js
@@ -0,0 +1,13 @@
+/**
+ * js responsavel pelo cadastro
+*/
+const gravar = () => {
+    // importando o axios para poder utiliza-lo
+    const axios = require('axios').default;
+
+    // buscando os dados do form
+    const id = document.querySelector("#id").value;
+    const descricao = document.querySelector("#iddesc").value;
+    const saldo = document.querySelector("#idsaldo").value;
+    const preco = document.querySelector("#idpreco").value;
+}
